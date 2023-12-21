2a14f74ef359ecdffac0acb33f5bdb67bcdd5f6f
IqbarAlam <nizemaya@gmail.com>
2023-12-22 02:47:01 +0700

    yang pertama commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Lotus</title>
+    <!-- Tambahkan link CSS jika diperlukan -->
+
+    
+{{-- <style>
+    .slideshow-container {
+    max-width: 1000px;
+    position: relative;
+    margin: auto;
+}
+
+.mySlides {
+    display: none;
+}
+
+.next {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    padding: 16px;
+    margin-top: -22px;
+}
+</style> --}}
+</head>
+<body>
+    <h1>Selamat datang di Hotel Lotus</h1>
+    <p>Ini adalah halaman utama Hotel Lotus.</p>
+
+    <!-- Slideshow Container -->
+    <div class="slideshow-container">
+        <div class="mySlides">
+            <!-- Isi slide pertama -->
+        </div>
+
+        <div class="mySlides">
+            <!-- Isi slide kedua -->
+        </div>
+
+        <!-- Tambahkan lebih banyak slide sesuai kebutuhan -->
+
+        <!-- Tombol Next -->
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+
+    <!-- Tambahkan script JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="your-custom-script.js"></script> <!-- Gantilah dengan nama file script Anda -->
+</body>
+</html>
